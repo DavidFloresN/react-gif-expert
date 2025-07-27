280b0c05188adacc5af6a654db37f227bf47109a
DavidFloresN <davidfloresnoh@gmail.com>
2025-07-26 19:18:33 -0600

    correccion para github-page

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-gif-expert/',
+  base: 'https://https://davidfloresn.github.io/react-gif-expert/',
 })
